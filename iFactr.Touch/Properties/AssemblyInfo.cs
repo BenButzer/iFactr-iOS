e555c20ed90d8c36b2380c9b23ccdf87c44e4ecf
Samuel Lippert <SLippert@zebra.com>
2017-08-15 15:08:11 -0500

    Add Verify and Package steps

--- iFactr.Touch/Properties/AssemblyInfo.cs
+++ iFactr.Touch/Properties/AssemblyInfo.cs
@@ -32,4 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("10.0.0.1225")]
+[assembly: AssemblyVersion("10.0.0.42")]
+[assembly: AssemblyInformationalVersion("Branch - master (Hash: Working Tree)")]
